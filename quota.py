abc377be5b0e9c1ec705bf5606bf6bcedfb33fdf
Tiago Magnus <tiagomagnus2010@hotmail.com>
2023-02-26 16:34:31 -0300

    Deletando data_manager, tcp, quota e udp

--- quota.py
+++
@@ -1,9 +0,0 @@
-from data_manager import DataManager
-
-class QuotaMonitor(DataManager):
-    tags = ['ifInOctets']
-    headers = ['Entry Quota']
-
-    def __init__(self, session):
-        self.session = session
-        super().__init__(session, QuotaMonitor.tags)
