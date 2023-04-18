cfd1f7ccd75043c85d912c6be6423862e36780dd
Renato Silveira <renpoa@gmail.com>
2023-04-18 03:27:15 -0300

    add compress

--- stats_window.py
+++ stats_window.py
@@ -49,41 +49,41 @@ class StatsWindow(QDialog):
     def initUI(self):
         self.vbLayout = QVBoxLayout()
         self.lblGraphInstant = QLabel('Gráfico de consumo instantâneo:')
-        self.lblGraphPerfil = QLabel('Gráfico de regressão linear em 6 tempos:')
-        self.lblGraphPerfil2 = QLabel('Grafico de regressão linear cumulativo:')
+        self.lblRegressShort = QLabel('Gráfico de regressão linear em 15 segundos:')
+        self.lblRegressTotal = QLabel('Grafico de regressão linear cumulativo:')
+        self.lblRegressCompress = QLabel('Grafico comprimido:')
 
         self.vbLayout.addWidget(self.lblGraphInstant)
-
+        
         self.canvas = MplCanvas(self, width=5, height=8, dpi=100)
 
         self.vbLayout.addWidget(self.canvas)
         self.vbLayout.addStretch(1)
 
-        self.vbLayout.addWidget(self.lblGraphPerfil)
+        
 
-        self.perfil_canvas = MplCanvas(self, width=5, height=8, dpi=100)
-        self.vbLayout.addWidget(self.perfil_canvas)
+        self.vbLayout.addWidget(self.lblRegressShort)
 
-        self.vbLayout.addWidget(self.lblGraphPerfil2)
-        self.perfil2_canvas = MplCanvas(self, width=5, height=8, dpi=100)
-        self.vbLayout.addWidget(self.perfil2_canvas)
+        self.regress_short_canvas = MplCanvas(self, width=5, height=8, dpi=100)
+        self.vbLayout.addWidget(self.regress_short_canvas)
 
+        self.vbLayout.addWidget(self.lblRegressTotal)
+        self.regress_long_canvas = MplCanvas(self, width=5, height=8, dpi=100)
+        self.vbLayout.addWidget(self.regress_long_canvas)
 
-        self.firstTime = 0
-        self.index = 0
-        self.xdata = []
-        self.dataModel = []
-
+        self.vbLayout.addWidget(self.lblRegressCompress)
+        self.regress_compress_canvas = MplCanvas(self, width=5, height=8, dpi=100)
+        self.vbLayout.addWidget(self.regress_compress_canvas)
 
         self.setLayout(self.vbLayout)
+        self.firstTime = 1
+        self.compress = []
 
 
     def update_plot(self):
-        # Drop off the first y element, append a new one.
-        #self.ydata = self.ydata[1:] + [random.randint(0, 10)]
-        #self.zdata = self.zdata[1:] + [random.randint(0, 5)]
 
-        frequency = 6
+        times_short = 3
+        times_long = 36
 
         time_stamp = []
         dataTotal = []       
@@ -108,63 +108,83 @@ class StatsWindow(QDialog):
                time_diff = int(self.agent.data_map['ifInOctets'][x]['timestamp']) - int(self.agent.data_map['ifInOctets'][x-1]['timestamp'])
                dataTotal.append((1/(10*time_diff))*8*(data))
 
-        if len(self.agent.data_map['ifInOctets']) > frequency:
-            dataIA = []
+        if len(self.agent.data_map['ifInOctets']) > times_short:
+            regress_short_data = []
             index = 0
-            xdata = []
+            x_regress_short = []
 
-            if((len(self.agent.data_map['ifInOctets']) % frequency) == 0):
-                for x in range(len(self.agent.data_map['ifInOctets']) - (frequency + 1), len(self.agent.data_map['ifInOctets'])):
+            if((len(self.agent.data_map['ifInOctets']) % times_short) == 0):
+                for x in range(len(self.agent.data_map['ifInOctets']) - (times_short + 1), len(self.agent.data_map['ifInOctets'])):
                     dataIn = int(self.agent.data_map['ifInOctets'][x]['value']) - int(self.agent.data_map['ifInOctets'][x-1]['value'])
                     dataOut = int(self.agent.data_map['ifInOctets'][x]['value']) - int(self.agent.data_map['ifInOctets'][x-1]['value'])
                     data = dataIn + dataOut
                     time_diff = int(self.agent.data_map['ifInOctets'][x]['timestamp']) - int(self.agent.data_map['ifInOctets'][x-1]['timestamp'])
-                    dataIA.append((1/(10*time_diff))*8*(data))
-                    xdata.append(float(index))
+                    regress_short_data.append((1/(10*time_diff))*8*(data))
+                    x_regress_short.append(float(index))
                     index += 1
-                
-                # print('-----------------------')
-                # print('x = ', xdata)                
-                # print('y = ', dataIA)
 
-                slope, intercept, r, p, std_err = stats.linregress(xdata, dataIA)
+                slope, intercept, r, p, std_err = stats.linregress(x_regress_short, regress_short_data)
                 
-                # print('r = ', r)
-                # print('p = ', p)
-                # print('std_err = ', std_err)
-                # print('-----------------------')
-
-                def myfunc(x):
+                def regressFun(x):
                     return slope * x + intercept
 
-                mymodel = list(map(myfunc, xdata))
+                short_regress_model = list(map(regressFun, x_regress_short))
+
+                for x in range(len(x_regress_short)):
+                    x_regress_short[x] = -15 + x_regress_short[x]*5
                 
-                if(self.firstTime == 0):
-                    self.dataModel.append(mymodel[0])
-                    self.dataModel.append(mymodel[len(mymodel) - 1])
-                    self.firstTime = 1
+                #Gráfico de consumo dos ultimos times_short*5s segundos
+                self.regress_short_canvas.axes.cla()  # Clear the canvas.                
+                self.regress_short_canvas.axes.yaxis.label.set_text('kbps')
+                self.regress_short_canvas.axes.grid(True, color='k', linestyle='-', linewidth=0.05)
+                self.regress_short_canvas.axes.plot(x_regress_short, short_regress_model, 'r')                
+                #self.regress_short_canvas.axes.plot(x_regress_short, regress_short_data, 'b')
+                self.regress_short_canvas.draw()
 
-                #print(len(mymodel))
+        if len(self.agent.data_map['ifInOctets']) > times_long:
+            regress_long_data = []
+            index = 0
+            x_regress_long = []
+
+            if((len(self.agent.data_map['ifInOctets']) % times_long) == 0):
+                for x in range(len(self.agent.data_map['ifInOctets']) - (times_long + 1), len(self.agent.data_map['ifInOctets'])):
+                    dataIn = int(self.agent.data_map['ifInOctets'][x]['value']) - int(self.agent.data_map['ifInOctets'][x-1]['value'])
+                    dataOut = int(self.agent.data_map['ifInOctets'][x]['value']) - int(self.agent.data_map['ifInOctets'][x-1]['value'])
+                    data = dataIn + dataOut
+                    time_diff = int(self.agent.data_map['ifInOctets'][x]['timestamp']) - int(self.agent.data_map['ifInOctets'][x-1]['timestamp'])
+                    regress_long_data.append((1/(10*time_diff))*8*(data))
+                    x_regress_long.append(float(index))
+                    index += 1
+
+                slope, intercept, r, p, std_err = stats.linregress(x_regress_long, regress_long_data)
                 
-                #Gráfico de consumo de perfil
-                self.perfil_canvas.axes.cla()  # Clear the canvas.
-                self.perfil_canvas.axes.yaxis.label.set_text('kbps')
-                self.perfil_canvas.axes.grid(True, color='k', linestyle='-', linewidth=0.05)
-                self.perfil_canvas.axes.plot(xdata, mymodel, dataIA, 'b')
+                def regressFun(x):
+                    return slope * x + intercept
 
-                self.perfil_canvas.draw()
+                long_regress_model = list(map(regressFun, x_regress_long))
 
+                
+                if self.firstTime == 1 :
+                    self.compress.append(long_regress_model[0])
+                    self.compress.append(long_regress_model[len(long_regress_model) - 1])
+                    self.firstTime = 0
+
+                if len(self.compress) > 2:
+                    x_long_canvas = []
+                    for x in range(len(self.compress)):
+                        x_long_canvas.append(x)
+                
+                    #Gráfico de consumo dos ultimos times_long*5s segundos
+                    self.regress_compress_canvas.axes.cla()  # Clear the canvas.                
+                    self.regress_compress_canvas.axes.yaxis.label.set_text('kbps')
+                    self.regress_compress_canvas.axes.grid(True, color='k', linestyle='-', linewidth=0.05)
+                    self.regress_compress_canvas.axes.plot(x_long_canvas, self.compress, 'b')                
+                    #self.regress_compress_canvas.axes.plot(x_regress_long, regress_long_data, 'b')
+                    self.regress_compress_canvas.draw()
+                
             else:
-                self.firstTime = 0
-                
+                self.firstTime = 1
 
-
-        # print('size ---------------------------')
-        # print(len(self.agent.data_map['ifInOctets']))
-        # print('time ---------------------------')
-        # print(time_stamp)
-        # print('data ---------------------------')
-        # print(dataTotal)
         #Gráfico de consumo instantâneo
         self.canvas.axes.cla()  # Clear the canvas.
         date_fmt = mdates.DateFormatter('%Y-%m-%d %H:%M:%S')
@@ -179,16 +199,32 @@ class StatsWindow(QDialog):
 
         self.canvas.draw()
 
-        if(len(self.dataModel) > 2) :
+        #Faz a regressão para todos os dados
+        if(len(self.agent.data_map['ifInOctets']) > 2) :            
 
-            self.perfil2_canvas.axes.cla()
+            self.regress_long_canvas.axes.cla()
             x_perfil2 = []
-            for x in range(len(self.dataModel)):
+            y_perfil2 = []
+            for x in range(len(self.agent.data_map['ifInOctets'])):
                 x_perfil2.append(x)
+                dataIn = int(self.agent.data_map['ifInOctets'][x]['value']) - int(self.agent.data_map['ifInOctets'][x-1]['value'])
+                dataOut = int(self.agent.data_map['ifInOctets'][x]['value']) - int(self.agent.data_map['ifInOctets'][x-1]['value'])
+                data = dataIn + dataOut
+                time_diff = int(self.agent.data_map['ifInOctets'][x]['timestamp']) - int(self.agent.data_map['ifInOctets'][x-1]['timestamp'])
+                y_perfil2.append((1/(10*time_diff))*8*(data))
             
-            self.perfil2_canvas.axes.grid(True, color='k', linestyle='-', linewidth=0.05)
-            self.perfil2_canvas.axes.plot(x_perfil2, self.dataModel, 'b')
-            self.perfil2_canvas.draw()
+            slope, intercept, r, p, std_err = stats.linregress(x_perfil2, y_perfil2)
+
+            def myfunc(x):
+                    return slope * x + intercept
+
+            mymodel = list(map(myfunc, x_perfil2))
+                        
+            self.regress_long_canvas.axes.grid(True, color='k', linestyle='-', linewidth=0.05)
+            self.regress_long_canvas.axes.plot(x_perfil2, mymodel, 'g')
+            self.regress_long_canvas.axes.yaxis.label.set_text('kbps')
+            # self.regress_long_canvas.axes.plot(x_perfil2, y_perfil2, 'b')
+            self.regress_long_canvas.draw()
 
 
 
